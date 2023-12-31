aec588faa44f8ae4f42cb4058e8f91feaf340ab6
sahiljassal <sahiljassal9198@gmail.com>
2024-01-01 00:39:51 +0530

    custom serializer for camel to snake case conversion finished

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\DTO\LowestPriceEnquiry;
+use App\Service\Serializer\DTOSerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,7 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ProductController extends AbstractController
 {
     #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
-    public function lowestPrice(Request $request, int $id, SerializerInterface $serializer): JsonResponse
+    public function lowestPrice(Request $request, int $id, DTOSerializer $serializer): Response
     {
 
         if ($request->headers->has('force-fail')) {
@@ -24,16 +25,18 @@ class ProductController extends AbstractController
 
         //change upcoming json data from request to class properties data.
         /** @var LowestPriceEnquiry $lowestPriceEnquiry */
-        $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
+        $lowestPriceEnquiry = $serializer->deserialize(
+            $request->getContent(), LowestPriceEnquiry::class, 'json'
+        );
         
         $lowestPriceEnquiry->setDiscountedPrice(50);
         $lowestPriceEnquiry->setPrice(100);
         $lowestPriceEnquiry->setPromotionId(3);
         $lowestPriceEnquiry->setPromotionName('Black friday half price sale');
         
-        // dd($lowestPriceEnquiry);
+        $responseContent = $serializer->serialize($lowestPriceEnquiry, 'json');
 
-        return new JsonResponse($lowestPriceEnquiry, 200);
+        return new Response($responseContent, 200);
     }
 
     #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
