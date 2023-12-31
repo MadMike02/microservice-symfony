6ce0795efcfd6f2a9b5b1a518aa2b705caf941cf
sahiljassal <sahiljassal9198@gmail.com>
2024-01-01 00:07:40 +0530

    data transfer object with json serializer and symfony deserializer

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,35 +2,38 @@
 
 namespace App\Controller;
 
+use App\DTO\LowestPriceEnquiry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductController extends AbstractController
 {
     #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
-    public function lowestPrice(Request $request, int $id):JsonResponse{
-        
-        if($request->headers->has('force-fail')){
+    public function lowestPrice(Request $request, int $id, SerializerInterface $serializer): JsonResponse
+    {
+
+        if ($request->headers->has('force-fail')) {
             return new JsonResponse([
                 'error' => 'something went wrong'
             ], $request->headers->get('force_fail'));
         }
 
+        //change upcoming json data from request to class properties data.
+        /** @var LowestPriceEnquiry $lowestPriceEnquiry */
+        $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
+        
+        $lowestPriceEnquiry->setDiscountedPrice(50);
+        $lowestPriceEnquiry->setPrice(100);
+        $lowestPriceEnquiry->setPromotionId(3);
+        $lowestPriceEnquiry->setPromotionName('Black friday half price sale');
+        
+        // dd($lowestPriceEnquiry);
 
-        return new JsonResponse([
-            "quantity" => 5,
-            "request_location" => "UK",
-            "voucher_code" => "QU812",
-            "request_date" => "2022-1-4",
-            "product_id" => $id,
-            "price" => 100,
-            "discounted_price" => 50,
-            "promotiond_id" => 3,
-            "promotion_name" => 'Black friday half price sale'
-        ], 200);
+        return new JsonResponse($lowestPriceEnquiry, 200);
     }
 
     #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
